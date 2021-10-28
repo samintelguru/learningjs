d27792d67e42311318d581105bf9c08268184f0b
samintelguru <vrosssam@gmail.com>
2021-10-28 03:57:12 +0300

    (chore) learning variables and display output on the console.

---
+++ intro.js
@@ -0,0 +1,25 @@
+//my first js code. This is a comment and will not be executed by the computer
+//variable - data storage for any languange. in Js variables are declared using the key word var or let.
+var name;
+var age;
+let job;
+//assign values to  variables use an equal sign
+name = "Sam"; //this is a string. a string is alphabetical text
+age = 34; //this is a number
+job = "Dev";
+
+//declare a variable and assign a value in one statement
+let friend = "Mufasa";
+let employmentStatus = true; 
+
+//types of variables eg string - text, numbers, boolean - true false, float - decimals
+//read more on w3schools site.
+
+let x = 10;
+let y = 5;
+let sum = x + y;
+// console.log(sum);
+
+if (age > 29){
+    console.log(" you are a millenial");
+}

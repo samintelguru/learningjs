d279ec65bdaa6c40bec77e99ae31f9ab706f0d26
samintelguru <vrosssam@gmail.com>
2021-10-30 02:29:48 +0300

    (ft) learning about for loops

---
+++ forloops.js
@@ -0,0 +1,21 @@
+//a for loop is an iteration technique. to iterate is to repeate
+//structure  for(when to start the loop;a condition to keep the loop running.; step in between each loop){code to be repeated/iterated;}
+//a simple loop to count numbers from 1 to 10
+//js uses a counter to keep track of the number of times an action is to be iterated. a counter is a number varibale.
+// for (let counter = 1; counter <= 10; counter++){
+//     console.log(counter);
+// }
+
+// for (let x = 10; x >=1; x--){
+//     console.log(x);
+// }
+
+for (let y = 2; y <=10; y++){
+    console.log(y);
+    y++;
+
+}
+
+//print numbers from 20 to 1
+//print odd numbers from 20 to 1 and in between 30 and 50
+
